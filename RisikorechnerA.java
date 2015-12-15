f43d567dab07a20f25d0ab08e4dbac24639d785e
moinmoin001 <janscheel@yahoo.de>
2015-12-15 16:58:35 +0100

    Aktien Risikomanagement

--- RisikorechnerA.java
+++ RisikorechnerA.java
@@ -1,21 +1,16 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import java.lang.Math;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.Component;
-import javax.swing.Box;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
 public class RisikorechnerA extends JFrame {
 
 	private JPanel contentPane;
@@ -23,12 +18,12 @@ public class RisikorechnerA extends JFrame {
 	private JTextField txtRisikoIn;
 	private JTextField txtAktienKurs;
 	private JTextField txtStoploss;
-	private JTextField txtRisikoIn_1;
-	private JTextField txtMaxAnzahl;
-	private JTextField txtGesamtKosten;
-	private JTextField RisikoEuAus;
-	private JTextField MaxAnzahlAus;
-	private JTextField GesamtKostenAus;
+	private JLabel lblGesammtAusgaben;
+	private JLabel lblErgebnisse;
+	private JLabel lblDepotwert;
+	private JLabel MaxAnzahlAus;
+	private JLabel RisikoEuAus;
+	private JLabel GesamtKostenAus;
 
 	/**
 	 * Launch the application.
@@ -54,77 +49,87 @@ public class RisikorechnerA extends JFrame {
 		
 	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 544, 456);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 			
 		txtDepotwert = new JTextField();
-		txtDepotwert.setText("Depotwert");
-		txtDepotwert.setBounds(10, 11, 86, 20);
+		txtDepotwert.setBounds(10, 25, 86, 20);
 		contentPane.add(txtDepotwert);
 		txtDepotwert.setColumns(10);
 		
 		txtRisikoIn = new JTextField();
-		txtRisikoIn.setText("Risiko in %");
-		txtRisikoIn.setBounds(10, 42, 86, 20);
+		txtRisikoIn.setBounds(10, 70, 86, 20);
 		contentPane.add(txtRisikoIn);
 		txtRisikoIn.setColumns(10);
 		
 		txtAktienKurs = new JTextField();
-		txtAktienKurs.setText("Aktien Kurs");
-		txtAktienKurs.setBounds(10, 73, 86, 20);
+		txtAktienKurs.setBounds(10, 116, 86, 20);
 		contentPane.add(txtAktienKurs);
 		txtAktienKurs.setColumns(10);
 		
 		txtStoploss = new JTextField();
-		txtStoploss.setText("StopLoss");
-		txtStoploss.setBounds(10, 104, 86, 20);
+		txtStoploss.setBounds(10, 164, 86, 20);
 		contentPane.add(txtStoploss);
 		txtStoploss.setColumns(10);
 		
 		JButton berechnen = new JButton("Berechnen");
-		berechnen.setBounds(10, 135, 101, 23);
+		berechnen.setBounds(10, 228, 101, 23);
 		contentPane.add(berechnen);
 		
-		txtRisikoIn_1 = new JTextField();
-		txtRisikoIn_1.setEditable(false);
-		txtRisikoIn_1.setText("Risiko in \u20AC");
-		txtRisikoIn_1.setBounds(99, 42, 86, 20);
-		contentPane.add(txtRisikoIn_1);
-		txtRisikoIn_1.setColumns(10);
+		JLabel lblRisikoIn = new JLabel("Risiko in %");
+		lblRisikoIn.setBounds(10, 56, 86, 14);
+		contentPane.add(lblRisikoIn);
 		
-		txtMaxAnzahl = new JTextField();
-		txtMaxAnzahl.setEditable(false);
-		txtMaxAnzahl.setText("Max Anzahl");
-		txtMaxAnzahl.setBounds(99, 73, 86, 20);
-		contentPane.add(txtMaxAnzahl);
-		txtMaxAnzahl.setColumns(10);
+		JLabel iblRisikoEuAus = new JLabel("Risiko in \u20AC");
+		iblRisikoEuAus.setBounds(183, 42, 86, 14);
+		contentPane.add(iblRisikoEuAus);
 		
-		txtGesamtKosten = new JTextField();
-		txtGesamtKosten.setEditable(false);
-		txtGesamtKosten.setText("Ausgaben");
-		txtGesamtKosten.setBounds(99, 104, 86, 20);
-		contentPane.add(txtGesamtKosten);
-		txtGesamtKosten.setColumns(10);
+		JLabel lblAktienkurs = new JLabel("Aktienkurs");
+		lblAktienkurs.setBounds(10, 101, 86, 14);
+		contentPane.add(lblAktienkurs);
 		
-		RisikoEuAus = new JTextField();
-		RisikoEuAus.setEditable(false);
-		RisikoEuAus.setBounds(183, 42, 86, 20);
-		contentPane.add(RisikoEuAus);
-		RisikoEuAus.setColumns(10);
+		JLabel lblStoploss = new JLabel("StopLoss");
+		lblStoploss.setBounds(10, 152, 86, 14);
+		contentPane.add(lblStoploss);
 		
-		MaxAnzahlAus = new JTextField();
-		MaxAnzahlAus.setEditable(false);
-		MaxAnzahlAus.setColumns(10);
-		MaxAnzahlAus.setBounds(183, 73, 86, 20);
+		JLabel lblMaxAnzahl = new JLabel("Max Anzahl");
+		lblMaxAnzahl.setBounds(183, 87, 86, 14);
+		contentPane.add(lblMaxAnzahl);
+		
+		lblGesammtAusgaben = new JLabel("Gesammt Ausgaben");
+		lblGesammtAusgaben.setBounds(183, 135, 121, 14);
+		contentPane.add(lblGesammtAusgaben);
+		
+		lblErgebnisse = new JLabel("Ergebnisse");
+		lblErgebnisse.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblErgebnisse.setBounds(183, 11, 121, 29);
+		contentPane.add(lblErgebnisse);
+		
+		lblDepotwert = new JLabel("Depotwert");
+		lblDepotwert.setBounds(10, 11, 86, 14);
+		contentPane.add(lblDepotwert);
+		
+		JLabel lblDeveloperJanScheel = new JLabel("All Rights Reserved by Jan Scheel");
+		lblDeveloperJanScheel.setBounds(10, 404, 203, 14);
+		contentPane.add(lblDeveloperJanScheel);
+		
+		JLabel lblCopyright = new JLabel("\u00A9 Copyright 2015");
+		lblCopyright.setBounds(384, 404, 134, 14);
+		contentPane.add(lblCopyright);
+		
+		MaxAnzahlAus = new JLabel();
+		MaxAnzahlAus.setBounds(183, 101, 121, 35);
 		contentPane.add(MaxAnzahlAus);
 		
-		GesamtKostenAus = new JTextField();
-		GesamtKostenAus.setEditable(false);
-		GesamtKostenAus.setColumns(10);
-		GesamtKostenAus.setBounds(183, 104, 86, 20);
+		RisikoEuAus = new JLabel();
+		RisikoEuAus.setBounds(183, 56, 121, 34);
+		contentPane.add(RisikoEuAus);
+		
+		GesamtKostenAus = new JLabel();
+		GesamtKostenAus.setBounds(183, 152, 121, 32);
 		contentPane.add(GesamtKostenAus);
 		
 		
@@ -137,40 +142,33 @@ public class RisikorechnerA extends JFrame {
 
 		public void actionPerformed(ActionEvent event) {
 			
-			try 
-			{
-				double Depotwert = Double.parseDouble(txtDepotwert.getText());
-				double RisikoPro = Double.parseDouble(txtRisikoIn.getText());
-				double Kurs = Double.parseDouble(txtAktienKurs.getText());
-				double StopLoss = Double.parseDouble(txtStoploss.getText());
+							//char[] array1 = txtAktienKurs.toCharArray(txtAktienKurs.getText);
+
+			
+			
+				double Depotwert = Double.parseDouble(txtDepotwert.getText().replace(",", "."));
+				double RisikoPro = Double.parseDouble(txtRisikoIn.getText().replace(",", "."));
+				double Kurs = Double.parseDouble(txtAktienKurs.getText().replace(",", "."));
+				double StopLoss = Double.parseDouble(txtStoploss.getText().replace(",", "."));
 				
-					double RisikoEu = (Depotwert*RisikoPro);
-					double MaxAnzahl = (RisikoEu/Kurs-StopLoss);
+					double RisikoEu = (RisikoPro/100*Depotwert);
+					double MaxAnzahl = (RisikoEu/(Kurs-StopLoss));
 					double GesammtAusgaben =(MaxAnzahl*Kurs);
 					
 					MaxAnzahl = Math.round(MaxAnzahl);
+					GesammtAusgaben = Math.round(GesammtAusgaben);
 					
-					RisikoEuAus.setText("" +RisikoEu);
+
+					
+					
+					RisikoEuAus.setText("" + RisikoEu);
 					MaxAnzahlAus.setText("" + MaxAnzahl);
 					GesamtKostenAus.setText("" + GesammtAusgaben);
 					
-			} catch (Exception e) {
-				JOptionPane.showInternalMessageDialog(null, "ERROR!");	
-			}
+		
 			
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+		
 		}
 	}
 }
